5fdcaf3a2ba2615d10346a6eb1b6eeab6ad903d6
Sankha Basak <66815559+SabarnaBasak-web@users.noreply.github.com>
2022-10-13 23:07:10 +0530

    Refactored Works.js file (#2)

--- src/Components/Works.js
+++ src/Components/Works.js
@@ -1,56 +1,36 @@
 import React from 'react';
 
+import { cardData } from '../assets/Data/Data';
 
-function Works({CraftomaniaImg, EnergysolutionImg, SocietyImg, BoxShadow}) {
-
+function Works() {
     return (
         <div className="work-container">
             <h1 className="work-heading">My works </h1>
             <div className="work-content">
-                <div className="card">
-                    <div className="card-heading">
-                        Society of the Saviour <br /> <a href="https://www.thesocietyofthesaviour.org.in" className="card-link" target="_blank" rel="noreferrer" > www.thesocietyofthesaviour.org.in </a>
-                    </div>
-                    <div className="card-body">
-                        <img src={SocietyImg} alt="societyofthesaviour" />
-                        <p className="card-body_content">
-                            Website of a NGO school based in Kolkata
-                        </p>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card-heading">
-                        Craftomania <br /><a href="https://www.craftomania.in" target="_blank" rel="noreferrer" className="card-link" > www.craftomania.in </a>
-                    </div>
-                    <div className="card-body">
-                        <img src={CraftomaniaImg} alt="craftomania " />
-                        <p className="card-body_content">
-                            An ecommerce website for handicrafts and boutique items
-                        </p>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card-heading">
-                        CSS Box shadow generator <br /><a href="https://my-project-1579578093491.web.app/" target="_blank" rel="noreferrer" className="card-link"> https://my-project-1579578093491.web.app/ </a>
-                    </div>
-                    <div className="card-body" >
-                        <img src={BoxShadow} alt="portfolio" />
-                        <p className="card-body_content">
-                            Box shadow generator for css elements
-                        </p>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card-heading">
-                        Energy Solutions <br /><a href="https://www.energysolutions.net.in" className="card-link"  target="_blank" rel="noreferrer"> www.energysolutions.net.in </a>
-                    </div>
-                    <div className="card-body" >
-                        <img src={EnergysolutionImg} alt="portfolio" />
-                        <p className="card-body_content">
-                            Website of a company specializes in manufacturing of Steam Boilers, hot water Generators, and Thermic fluid heaters
-                        </p>
-                    </div>
-                </div>
+                {cardData.map((item) => {
+                    return (
+                        <div className="card" key={item.id}>
+                            <div className="card-heading">
+                                {item.title}
+                                <br />
+                                <a
+                                    href={item.link}
+                                    className="card-link"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    {item.link}
+                                </a>
+                            </div>
+                            <div className="card-body">
+                                <img src={item.image} alt="{item.title}" />
+                                <p className="card-body_content">
+                                    {item.content}
+                                </p>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
---
+++ src/assets/Data/Data.js
@@ -0,0 +1,35 @@
+import CraftomaniaImg from '../images/craftomania.JPG';
+import EnergysolutionImg from '../images/energysolutions.JPG';
+import SocietyImg from '../images/societyofthesav.JPG';
+import BoxShadow from '../images/cssboxshadow.JPG';
+
+export const cardData = [
+    {
+        id: 'data-1',
+        title: 'Society of the Saviour',
+        link: 'https://www.thesocietyofthesaviour.org.in',
+        content: 'Website of a NGO school based in Kolkata',
+        image: SocietyImg,
+    },
+    {
+        id: 'data-2',
+        title: 'Craftomania',
+        link: 'https://www.craftomania.in',
+        content: 'An ecommerce website for handicrafts and boutique items',
+        image: CraftomaniaImg,
+    },
+    {
+        id: 'data-3',
+        title: 'CSS Box shadow generator',
+        link: 'https://cssboxshadowgen.netlify.app',
+        content: 'Box shadow generator for css elements',
+        image: BoxShadow,
+    },
+    {
+        id: 'data-4',
+        title: 'Energy Solutions',
+        link: 'https://www.energysolutions.net.in',
+        content: 'Website of a company specializes in manufacturing of Steam Boilers, hot water Generators, and Thermic fluid heaters',
+        image: EnergysolutionImg,
+    },
+]
